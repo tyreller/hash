36adc943db31b64c24635b66089c3cd5c7abd097
tyreller <thiagoyair1@gmail.com>
2023-10-22 04:14:17 -0300

    Hash done Iter missing

--- hash.go
+++ hash.go
@@ -6,8 +6,8 @@ import (
 )
 
 type parClaveValor[K comparable, V any] struct {
-	clave K
-	dato  V
+	clav K
+	dat  V
 }
 
 type hashAbierto[K comparable, V any] struct {
@@ -20,6 +20,19 @@ func convertirABytes[K comparable](clave K) []byte {
 	return []byte(fmt.Sprintf("%v", clave))
 }
 
+func (h *hashAbierto[K, V]) hashFuncIndice(clave K) int {
+	indiceBytes := convertirABytes(clave)
+
+	hashValue := 0
+	for i := 0; i < len(indiceBytes); i++ {
+		b := indiceBytes[i]
+		hashValue += int(b)
+	}
+
+	indice := hashValue % h.tam
+	return indice
+}
+
 //Tamaño variable
 // func CrearHash[K comparable, V any](tam int) Diccionario[K, V] {
 // 	tabla := make([]TDALista.Lista[parClaveValor[K, V]], tam)
@@ -30,7 +43,7 @@ func convertirABytes[K comparable](clave K) []byte {
 // 	}
 // }
 
-// Tamaño fijo en 151
+// Tamaño fijo en 151, numero primo
 func CrearHash[K comparable, V any]() Diccionario[K, V] {
 	tabla := make([]TDALista.Lista[parClaveValor[K, V]], 151)
 	return &hashAbierto[K, V]{
@@ -40,39 +53,80 @@ func CrearHash[K comparable, V any]() Diccionario[K, V] {
 	}
 }
 
-func Guardar(clave K, dato V) {
-
-}
-func Pertenece(clave K) bool {
-
-}
-func Obtener(clave K) V {
-
-}
-func Borrar(clave K) V {
-
-}
-func Cantidad() int {
-
-}
-func Iterar(func(clave K, dato V) bool) {
-
+func (h *hashAbierto[K, V]) Guardar(clave K, dato V) {
+	indice := h.hashFuncIndice(clave)
+	lista := h.tabla[indice]
+	par := parClaveValor[K, V]{clav: clave, dat: dato}
+	lista.InsertarUltimo(par)
+	h.cantidad++
 }
 
-func Iterador() IterDiccionario[K, V] {
+func (h *hashAbierto[K, V]) Pertenece(clave K) bool {
+	indice := h.hashFuncIndice(clave)
+	listaIter := h.tabla[indice].Iterador()
+
+	for listaIter.HaySiguiente() {
+		par := listaIter.VerActual()
+		if par.clav == clave {
+			return true
+		}
+		listaIter.Siguiente()
+	}
+	return false
+}
+
+func (h *hashAbierto[K, V]) Obtener(clave K) V {
+	indice := h.hashFuncIndice(clave)
+	listaIter := h.tabla[indice].Iterador()
+
+	for listaIter.HaySiguiente() {
+		par := listaIter.VerActual()
+		if par.clav == clave {
+			return par.dat
+		}
+		listaIter.Siguiente()
+	}
+	panic("La clave no pertenece al diccionario")
+}
+
+func (h *hashAbierto[K, V]) Borrar(clave K) V {
+	indice := h.hashFuncIndice(clave)
+	listaIter := h.tabla[indice].Iterador()
+
+	for listaIter.HaySiguiente() {
+		par := listaIter.VerActual()
+		if par.clav == clave {
+			listaIter.Borrar()
+			h.cantidad--
+			return par.dat
+		}
+		listaIter.Siguiente()
+	}
+	panic("La clave no pertenece al diccionario")
+}
+
+func (h *hashAbierto[K, V]) Cantidad() int {
+	return h.cantidad
+}
+
+func (h *hashAbierto[K, V]) Iterar(func(clave K, dato V) bool) {
 
 }
 
-func HaySiguiente() bool {
+func (h *hashAbierto[K, V]) Iterador() IterDiccionario[K, V] {
 
 }
-func VerActual() (K, V) {
+
+func (h *hashAbierto[K, V]) HaySiguiente() bool {
+
+}
+func (h *hashAbierto[K, V]) VerActual() (K, V) {
 	if VerActual == false {
 		panic("El iterador termino de iterar")
 	}
 
 }
-func Siguiente() {
+func (h *hashAbierto[K, V]) Siguiente() {
 	if VerActual == false {
 		panic("El iterador termino de iterar")
 	}
